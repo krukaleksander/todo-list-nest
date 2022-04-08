3a627188052b7d59c30c88e41c7e165bbfe50344
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-08 11:11:03 +0200

    feat: editItem

--- helpers/ToDoListClass.ts
+++ helpers/ToDoListClass.ts
@@ -3,6 +3,16 @@ import { IToDoItem } from 'interfaces/ToDoList';
 
 let db: IToDoItem[] = [];
 export class ToDoList {
+  editItem(id: string, newTitle: string) {
+    if (newTitle.length < 1)
+      return { statusCode: 500, msg: 'give me a new name for a task' };
+    db = db.map((task) => {
+      const { id, isDone } = task;
+      if (task.id === id) return { id, title: newTitle, isDone };
+      return task;
+    });
+    return this;
+  }
   changeDoneStatus(id: string) {
     db = db.map((task) => {
       const { id, title, isDone } = task;
