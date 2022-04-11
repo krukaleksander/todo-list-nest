5110a40d06fe6be17f7d5f8f4dbdadc5aa333178
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-11 09:21:49 +0200

    Implement add new task via endpoint

--- src/add/add.controller.ts
+++ src/add/add.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AddService } from './add.service';
 import { IToDoItem } from '../../interfaces/ToDoList';
+import { IRequest } from '../../interfaces/IRequest';
 
 @Controller('/add')
-export class AppController {
+export class AddController {
   constructor(private readonly addService: AddService) {}
 
   @Get('/')
-  addToDb(): IToDoItem {
-    return this.addService.addToDb();
+  addToDb(@Body() body: IRequest): IToDoItem[] {
+    return this.addService.addToDb(body);
   }
 }
--- src/add/add.service.ts
+++ src/add/add.service.ts
@@ -4,9 +4,9 @@ import { ToDoList } from '../../helpers/ToDoListClass';
 
 @Injectable()
 export class AddService {
-  addToDb(): IToDoItem {
+  addToDb({ title }): IToDoItem[] {
     const myList = new ToDoList();
-    myList.addNewItem('do the task');
-    const response = myList.showTaskList();
+    myList.addNewItem(title);
+    return myList.showTaskList();
   }
 }
